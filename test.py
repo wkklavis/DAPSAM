3157e026e49f92395b00951551c6872b5a6b90d2
wkklavis <67302399+wkklavis@users.noreply.github.com>
2024-01-30 16:58:40 +0800

    Update test.py

--- test.py
+++ test.py
@@ -27,24 +27,7 @@ def inference(args, epoch, snapshot_path, test_loader, model, test_save_path=Non
     metric_dict = ['Dice', 'ASD']
 
     last_name = None
-    # test1_prototype = []
-    # test1_di_prototype = []
-    # test2_prototype = []
-    # test2_di_prototype = []
-    # test3_prototype = []
-    # test3_di_prototype = []
-    # test4_prototype = []
-    # test4_di_prototype = []
-    # test5_prototype = []
-    # test5_di_prototype = []
-    # source_prototype = []
-    # source_di_prototype = []
-    # test1_weight = []
-    # test2_weight = []
-    # test3_weight = []
-    # test4_weight = []
-    # test5_weight = []
-    # source_weight = []
+
     with torch.no_grad():
         for batch, data in enumerate(tqdm(test_loader, position=0, leave=True, ncols=70)):
             x, y, path = data['data'], data['mask'], data['name']
@@ -59,31 +42,7 @@ def inference(args, epoch, snapshot_path, test_loader, model, test_save_path=Non
             x = x.cuda()
             seg_logit = model(x, False, args.img_size)
             seg_output = torch.sigmoid(seg_logit['masks'].detach().cpu())
-            ####################
-            # from thop import profile
-            # from fvcore.nn import FlopCountAnalysis, parameter_count_table
-            # # x = torch.randn(1, 256, 24, 24).float().cuda()
-            #
-            # trainable_params = 0
-            # all_param = 0
-            # for _, param in model.named_parameters():
-            #     all_param += param.numel()
-            #     if param.requires_grad:
-            #         trainable_params += param.numel()
-            # print(
-            #     f"trainable params: {trainable_params/1e6} || all params: {all_param/1e6}")
-            #
-            #
-            # macs, params = profile(model, inputs=(x,False, args.img_size))  # ,verbose=False
-            # print("MACs", macs)
-            # print("p", params)
-            #
-            # print("@@@@@@@@@@@@@@")
-            #
-            # # flops = FlopCountAnalysis(model, x)
-            # # print("FLOPs", flops.total())
-            # print(parameter_count_table(model))
-            ###################
+            
             if current_name != last_name:  # Calculate the previous 3D volume
                 metrics = calculate_metrics(seg_output3D, y3D)
                 for i in range(len(metrics)):
@@ -99,170 +58,6 @@ def inference(args, epoch, snapshot_path, test_loader, model, test_save_path=Non
                 seg_output3D = seg_output.unsqueeze(2)
                 y3D = y.unsqueeze(2)
 
-            # if ("RUNMC") in current_name[0] and len(source_di_prototype)<100:
-            #     source_prototype.append(model.prompt_generator.prototype.cpu().detach().numpy())
-            #     source_di_prototype.append(model.prompt_generator.di_prototype.cpu().detach().numpy())
-            #
-            # if ("BMC") in current_name[0] and len(test1_di_prototype)<100:
-            #     test1_prototype.append(model.prompt_generator.prototype.cpu().detach().numpy())
-            #     test1_di_prototype.append(model.prompt_generator.di_prototype.cpu().detach().numpy())
-            #
-            # if ("BIDMC") in current_name[0] and len(test2_di_prototype)<100:
-            #     test2_prototype.append(model.prompt_generator.prototype.cpu().detach().numpy())
-            #     test2_di_prototype.append(model.prompt_generator.di_prototype.cpu().detach().numpy())
-            #
-            # if ("UCL") in current_name[0] and len(test3_di_prototype)<100:
-            #     test3_prototype.append(model.prompt_generator.prototype.cpu().detach().numpy())
-            #     test3_di_prototype.append(model.prompt_generator.di_prototype.cpu().detach().numpy())
-            #
-            # if ("HK") in current_name[0] and len(test4_di_prototype)<100:
-            #     test4_prototype.append(model.prompt_generator.prototype.cpu().detach().numpy())
-            #     test4_di_prototype.append(model.prompt_generator.di_prototype.cpu().detach().numpy())
-            #
-            # if ("I2CVB") in current_name[0] and len(test5_di_prototype)<100:
-            #     test5_prototype.append(model.prompt_generator.prototype.cpu().detach().numpy())
-            #     test5_di_prototype.append(model.prompt_generator.di_prototype.cpu().detach().numpy())
-            #
-            # if len(test4_di_prototype)== 100 and len(test5_di_prototype)==100:
-            #     test1_prototype = np.array(test1_prototype)
-            #     test1_di_prototype = np.array(test1_di_prototype)
-            #
-            #     test2_prototype = np.array(test2_prototype)
-            #     test2_di_prototype = np.array(test2_di_prototype)
-            #
-            #     test3_prototype = np.array(test3_prototype)
-            #     test3_di_prototype = np.array(test3_di_prototype)
-            #
-            #     test4_prototype = np.array(test4_prototype)
-            #     test4_di_prototype = np.array(test4_di_prototype)
-            #
-            #     test5_prototype = np.array(test5_prototype)
-            #     test5_di_prototype = np.array(test5_di_prototype)
-            #
-            #     source_prototype = np.array(source_prototype)
-            #     source_di_prototype = np.array(source_di_prototype)
-            #
-            #     memory_bank = model.prompt_generator.memory_bank.weight.cpu().detach().numpy()
-            #
-            #     # all_data = np.vstack([test1_prototype, test2_prototype,
-            #     #                       test3_prototype, test4_prototype,
-            #     #                       test5_prototype, source_prototype ])
-            #     # all_data = np.vstack([test1_di_prototype, test2_di_prototype,
-            #     #                       test3_di_prototype, test4_di_prototype,
-            #     #                       test5_di_prototype, source_di_prototype, memory_bank])
-            #     all_data = np.vstack([test1_prototype, test1_di_prototype, test2_prototype, test2_di_prototype,
-            #                           test3_prototype, test3_di_prototype, test4_prototype, test4_di_prototype,
-            #                           test5_prototype, test5_di_prototype,  memory_bank])#source_prototype, source_di_prototype,
-            #
-            #     # 使用 t-SNE 进行降维
-            #     tsne = TSNE(n_components=2, random_state=42, perplexity=20)
-            #     tsne_result = tsne.fit_transform(all_data)
-            #     # 分离每个类别的 t-SNE 结果
-            #     tsne_class1 = tsne_result[:100]  # 假设每个类别有 100 个数据点
-            #     tsne_class2 = tsne_result[100:200]
-            #     tsne_class3 = tsne_result[200:300]
-            #     tsne_class4 = tsne_result[300:400]
-            #     tsne_class5 = tsne_result[400:500]
-            #     tsne_class6 = tsne_result[500:600]
-            #     tsne_class7 = tsne_result[600:700]
-            #     tsne_class8 = tsne_result[700:800]
-            #     tsne_class9 = tsne_result[800:900]
-            #     tsne_class10 = tsne_result[900:1000]
-            #
-            #     # tsne_class11 = tsne_result[1000:1100]
-            #     # tsne_class12 = tsne_result[1100:1200]
-            #
-            #     tsne_class11 = tsne_result[1000:1200]
-            #     # 绘制 t-SNE 图
-            #     plt.figure(figsize=(15, 10))
-            #     plt.scatter(tsne_class1[:, 0], tsne_class1[:, 1], label='BMC_Prototype', color='pink', alpha=0.7,)
-            #     plt.scatter(tsne_class2[:, 0], tsne_class2[:, 1], label='BMC_DA_Prototype', color='pink', marker = "^", alpha=0.7,)
-            #
-            #     plt.scatter(tsne_class9[:, 0], tsne_class9[:, 1], label='I2CVB_Prototype', color='purple',alpha=0.7)
-            #     plt.scatter(tsne_class10[:, 0], tsne_class10[:, 1], label='I2CVB_DA_Prototype', color='purple', marker = "^",alpha=0.7,)
-            #
-            #
-            #     plt.scatter(tsne_class5[:, 0], tsne_class5[:, 1], label='UCL_Prototype', color='green',alpha=0.7,)
-            #     plt.scatter(tsne_class6[:, 0], tsne_class6[:, 1], label='UCL_DA_Prototype', color='green', marker = "^", alpha=0.7,)
-            #
-            #     plt.scatter(tsne_class3[:, 0], tsne_class3[:, 1], label='BIDMC_Prototype', color='orange', alpha=0.7,)
-            #     plt.scatter(tsne_class4[:, 0], tsne_class4[:, 1], label='BIDMC_DA_Prototype', color='orange', marker = "^", alpha=0.7,)
-            #
-            #     plt.scatter(tsne_class7[:, 0], tsne_class7[:, 1], label='HK_Prototype', color='brown')
-            #     plt.scatter(tsne_class8[:, 0], tsne_class8[:, 1], label='HK_DA_Prototype', color='brown', marker = "^" ,alpha=0.7,)
-            #
-            #     # plt.scatter(tsne_class11[:, 0], tsne_class11[:, 1], label='Source_Prototype', color='olive')
-            #     # plt.scatter(tsne_class12[:, 0], tsne_class12[:, 1], label='Source_DA_Prototype', color='olive', marker = "^" ,alpha=0.7,)
-            #
-            #     plt.scatter(tsne_class11[:, 0], tsne_class11[:, 1], label='Memory_Bank_Prototype', color='gray',alpha=0.7,)
-            #
-            #     plt.title('tSNE Prototype Visualization', fontdict={'size': 18})
-            #     plt.legend()
-            #     plt.grid(True)
-            #     plt.savefig('./result/PrototypeVisualization20-1.png')
-            #     plt.show()
-            # if ("RUNMC") in current_name[0] and len(source_weight)<100:
-            #     source_weight.append(model.prompt_generator.memory_bank.att_weight.cpu().detach().numpy())
-            #
-            # if ("BMC") in current_name[0] and len(test1_weight)<100:
-            #     test1_weight.append(model.prompt_generator.memory_bank.att_weight.cpu().detach().numpy())
-            #
-            # if ("BIDMC") in current_name[0] and len(test2_weight)<100:
-            #     test2_weight.append(model.prompt_generator.memory_bank.att_weight.cpu().detach().numpy())
-            #
-            # if ("UCL") in current_name[0] and len(test3_weight)<100:
-            #     test3_weight.append(model.prompt_generator.memory_bank.att_weight.cpu().detach().numpy())
-            #
-            # if ("HK") in current_name[0] and len(test4_weight)<100:
-            #     test4_weight.append(model.prompt_generator.memory_bank.att_weight.cpu().detach().numpy())
-            #
-            # if ("I2CVB") in current_name[0] and len(test5_weight)<100:
-            #     test5_weight.append(model.prompt_generator.memory_bank.att_weight.cpu().detach().numpy())
-            #
-            # if len(test4_weight)== 100 and len(test5_weight)==100:
-            #     test1_weight = np.array(test1_weight)
-            #
-            #     test2_weight = np.array(test2_weight)
-            #
-            #     test3_weight = np.array(test3_weight)
-            #
-            #     test4_weight = np.array(test4_weight)
-            #
-            #     test5_weight = np.array(test5_weight)
-            #
-            #     source_weight = np.array(source_weight)
-            #
-            #     all_data = np.vstack([test1_weight, test2_weight,test3_weight,test4_weight,test5_weight,source_weight])
-            #     # 使用 t-SNE 进行降维
-            #     tsne = TSNE(n_components=2, random_state=42)
-            #     tsne_result = tsne.fit_transform(all_data)
-            #     # 分离每个类别的 t-SNE 结果
-            #     tsne_class1 = tsne_result[:100]  # 假设每个类别有 100 个数据点
-            #     tsne_class2 = tsne_result[100:200]
-            #     tsne_class3 = tsne_result[200:300]
-            #     tsne_class4 = tsne_result[300:400]
-            #     tsne_class5 = tsne_result[400:500]
-            #     tsne_class6 = tsne_result[500:600]
-            #
-            #
-            #     # 绘制 t-SNE 图
-            #     plt.figure(figsize=(15, 10))
-            #     plt.scatter(tsne_class1[:, 0], tsne_class1[:, 1], label='test1_weight', color='red')
-            #     plt.scatter(tsne_class2[:, 0], tsne_class2[:, 1], label='test2_weight', color='blue')
-            #     plt.scatter(tsne_class3[:, 0], tsne_class3[:, 1], label='test3_weight', color='green')
-            #     plt.scatter(tsne_class4[:, 0], tsne_class4[:, 1], label='test4_weight', color='yellow')
-            #
-            #     plt.scatter(tsne_class5[:, 0], tsne_class5[:, 1], label='test5_weight', color='purple')
-            #     plt.scatter(tsne_class6[:, 0], tsne_class6[:, 1], label='source_weight', color='black')
-            #
-            #     plt.title('t-SNE Visualization of 6 weights')
-            #     plt.legend()
-            #     plt.grid(True)
-            #     plt.savefig('./result/weight_tsne.png')
-            #     plt.show()
-            draw_output = (seg_output.detach().cpu().numpy() * 255).astype(np.uint8)
-            # cv2.imwrite(args.result_path + '/' + str(path[0]).split('/')[-1] + '-' + str(y3D.shape[2]) + '_pred.png',
-            #             draw_output[0][0])
             last_name = current_name
 
     # Calculate the last 3D volume
@@ -282,50 +77,6 @@ def inference(args, epoch, snapshot_path, test_loader, model, test_save_path=Non
     logging.info("Test Metrics: "+str(print_test_metric))
     return test_metrics_y
 
-def save_image(args, epoch, snapshot_path, test_loader, model, test_save_path=None):
-    print("\nSave images")
-    print("--" * 15)
-    last_name = None
-    with torch.no_grad():
-        for batch, data in enumerate(tqdm(test_loader, position=0, leave=True, ncols=70)):
-            x, y, path = data['data'], data['mask'], data['name']
-            current_name = path
-            if last_name is None:
-                last_name = path
-
-            x = torch.from_numpy(x).to(dtype=torch.float32)
-            y = torch.from_numpy(y).to(dtype=torch.float32)
-
-            x = x.cuda()
-            seg_logit = model(x, False, args.img_size)
-            seg_output = torch.sigmoid(seg_logit['masks'].detach().cpu())
-            if current_name != last_name:  # Calculate the previous 3D volume
-                metrics = calculate_metrics(seg_output3D, y3D)
-                del seg_output3D
-                del y3D
-
-            try:
-                seg_output3D = torch.cat((seg_output.unsqueeze(2), seg_output3D), 2)
-                y3D = torch.cat((y.unsqueeze(2), y3D), 2)
-            except:
-                seg_output3D = seg_output.unsqueeze(2)
-                y3D = y.unsqueeze(2)
-
-            output_path = os.path.join(snapshot_path, 'saved_imges',str(path[0]).split('/')[-2])
-            if not os.path.exists(output_path):
-                os.makedirs(output_path)
-            draw_output = (seg_output.detach().cpu().numpy() * 255).astype(np.uint8)
-            draw_x = (x.detach().cpu().numpy() * 255).astype(np.uint8)
-            draw_gt = (y.detach().cpu().numpy() * 255).astype(np.uint8)
-            cv2.imwrite(output_path + '/' + str(path[0]).split('/')[-1] + '-' + str(y3D.shape[2]) + '_pred.png',
-                        draw_output[0][0])
-            cv2.imwrite(output_path + '/' + str(path[0]).split('/')[-1] + '-' + str(y3D.shape[2]) + '_x.png',
-                        draw_x[0][0])
-            cv2.imwrite(output_path + '/' + str(path[0]).split('/')[-1] + '-' + str(y3D.shape[2]) + '_gt.png',
-                        draw_gt[0][0])
-            last_name = current_name
-    logging.info("saved")
-    return
 
 def config_to_dict(config):
     items_dict = {}
