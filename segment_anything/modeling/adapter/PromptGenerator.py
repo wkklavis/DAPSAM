9d8df57bc9a66c38ec82d2d802d0561733c515f0
wkklavis <67302399+wkklavis@users.noreply.github.com>
2024-01-30 16:44:05 +0800

    Update PromptGenerator.py

--- segment_anything/modeling/adapter/PromptGenerator.py
+++ segment_anything/modeling/adapter/PromptGenerator.py
@@ -40,9 +40,9 @@ class PromptGenerator(nn.Module):
                 nn.GELU(),
                 nn.Linear(self.embed_dim // self.scale_factor, self.embed_dim)
             )
-            # spatail_filter = ChannelFilter()
+            # channel_filter = ChannelFilter()
             setattr(self, 'lightweight_mlp_{}'.format(str(i)), lightweight_mlp)
-            # setattr(self, 'spatial_filter_{}'.format(str(i)), spatail_filter)
+            # setattr(self, 'channel_filter_{}'.format(str(i)), channel_filter)
 
         self.apply(self._init_weights)
     def _init_weights(self, m):
@@ -71,7 +71,7 @@ class PromptGenerator(nn.Module):
         # N, C, H, W = embedding_feature.shape
         feature = feature.permute(0, 3, 1, 2)
         N, C, H, W = feature.shape
-        # spatial_filter = getattr(self, 'spatial_filter_{}'.format(str(i)))
+        # channel_filter = getattr(self, 'channel_filter_{}'.format(str(i)))
         # filtered_feature = spatial_filter(0.5*feature+embedding_feature)
 
         # filtered_feature = filtered_feature.reshape(N, C, H * W).permute(0, 2, 1)
@@ -82,42 +82,4 @@ class PromptGenerator(nn.Module):
         prompt = lightweight_mlp(feature)#+iip 0.5*feature+handcrafted_feature ++embedding_contour 0.5*feature+embedding_feature
         return prompt
 
-    def forward(self, x):
-        if self.input_type == 'laplacian':
-            pyr_A = self.lap_pyramid.pyramid_decom(img=x, num=self.freq_nums)
-            x = pyr_A[:-1]
-            laplacian = x[0]
-            for x_i in x[1:]:
-                x_i = F.interpolate(x_i, size=(laplacian.size(2), laplacian.size(3)), mode='bilinear', align_corners=True)
-                laplacian = torch.cat([laplacian, x_i], dim=1)
-            x = laplacian
-        elif self.input_type == 'fft':
-            x = self.fft(x, self.freq_nums)
-        elif self.input_type == 'all':
-            x = self.prompt.unsqueeze(0).repeat(x.shape[0], 1, 1, 1)
-
-        # get prompting
-        prompt = self.prompt_generator(x)
-
-        if self.mode == 'input':
-            prompt = self.proj(prompt)
-            return prompt
-        elif self.mode == 'stack':
-            prompts = []
-            for i in range(self.depth):
-                proj = getattr(self, 'proj_{}'.format(str(i)))
-                prompts.append(proj(prompt))
-            return prompts
-        elif self.mode == 'hierarchical':
-            prompts = []
-            for i in range(self.depth):
-                proj_prompt = getattr(self, 'proj_prompt_{}'.format(str(i)))
-                prompt = proj_prompt(prompt)
-                prompts.append(self.proj_token(prompt))
-            return prompts
-
-
-
-
-
-
+    
